5a7d8bfef7f898257358e91a98820f2fe0978044
Yeon JeGyeong <yllh325@gmail.com>
2023-07-25 15:12:11 +0900

    Feat:customer bottomsheet 바텀시트 스와이프 라이브러리 사용해서 만드는중

--- square/src/App.js
+++ square/src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Header from "./routes/header";
 import Footer from "./routes/footer";
 import Main from "./routes/main";
+import BottomSheet from "./routes/BottomSheet";
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
 			<Main></Main>
+			<BottomSheet></BottomSheet>
 			<Footer />
 		</div>
 	);
---
+++ square/src/routes/BottomSheet.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import Drawer from '@mui/material/Drawer';
+import { useSwipeable } from 'react-swipeable';
+import '../App.css';
+
+export default function BottomSheet() {
+  const [state, setState] = React.useState({
+    bottom: false,
+  });
+
+  const handlers = useSwipeable({
+    onSwipedUp: () => {
+      setState({ ...state, bottom: true });
+    },
+    onSwipedDown: () => {
+      setState({ ...state, bottom: false });
+    },
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  return (
+    <div>
+      <React.Fragment key={'bottom'}>
+        <Drawer
+          anchor={'bottom'}
+          open={state.bottom}
+          onClose={toggleDrawer('bottom', false)}
+        >
+          <button>하이</button>
+          <button>하이</button>
+          <button>하이</button>
+          <button>하이</button>
+        </Drawer>
+      </React.Fragment>
+      <div className="swipe-handler" {...handlers}>
+        <div style={{ display: 'flex', justifyContent: 'center', gap: '5px' }}>
+          <div style={{ width: '4px', height: '4px', backgroundColor: 'darkgray', borderRadius: '50%' }} />
+          <div style={{ width: '4px', height: '4px', backgroundColor: 'darkgray', borderRadius: '50%' }} />
+          <div style={{ width: '4px', height: '4px', backgroundColor: 'darkgray', borderRadius: '50%' }} />
+        </div>
+      </div>
+    </div>
+  );
+}
