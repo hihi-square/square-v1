f7237195a1959b7032ded8e91957edcffc2dd666
soyeonnnb <la28s5d@naver.com>
2023-07-26 17:12:07 +0900

    Docs: controller 문구 변경

--- square/src/main/java/com/hihi/square/controller.java
+++ square/src/main/java/com/hihi/square/controller.java
@@ -9,8 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class controller {
 	@GetMapping
 	public ResponseEntity<String> test(){
-		System.out.println("들어왔다");
-		System.out.println("다시 실행");
-		return new ResponseEntity<>("성공 - b_soyeon", HttpStatus.OK);
+		System.out.println("merge test");
+		return new ResponseEntity<>("성공 - b_soyeon. merge-version", HttpStatus.OK);
 	}
 }
