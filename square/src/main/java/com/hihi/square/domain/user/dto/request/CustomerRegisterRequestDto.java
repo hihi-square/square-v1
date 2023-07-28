3f09091a61120d5f3e69426e4f3c277e43958814
soyeonnnb <la28s5d@naver.com>
2023-07-28 09:45:13 +0900

    Docs: enum 뒤에 Type 붙임

--- square/src/main/java/com/hihi/square/domain/user/dto/request/CustomerRegisterRequestDto.java
+++ square/src/main/java/com/hihi/square/domain/user/dto/request/CustomerRegisterRequestDto.java
@@ -7,9 +7,9 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import com.hihi.square.domain.user.entity.Customer;
-import com.hihi.square.domain.user.entity.UserRank;
-import com.hihi.square.domain.user.entity.UserSocialLogin;
-import com.hihi.square.domain.user.entity.UserStatus;
+import com.hihi.square.domain.user.entity.UserRankType;
+import com.hihi.square.domain.user.entity.UserSocialLoginType;
+import com.hihi.square.domain.user.entity.UserStatusType;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -44,13 +44,13 @@ public class CustomerRegisterRequestDto {
 			.name(name)
 			.phone(phone)
 			.email(email)
-			.rank(UserRank.valueOf(UserRank.UR01.name()))
+			.rank(UserRankType.valueOf(UserRankType.UR01.name()))
 			.marketingAgree(marketingAgree)
 			.createdAt(LocalDateTime.now())
 			.modifiedAt(LocalDateTime.now())
 			.lastLogin(LocalDateTime.now())
-			.status(UserStatus.valueOf(UserStatus.ST01.name()))
-			.social(UserSocialLogin.valueOf(UserSocialLogin.US01.name()))
+			.status(UserStatusType.valueOf(UserStatusType.ST01.name()))
+			.social(UserSocialLoginType.valueOf(UserSocialLoginType.US01.name()))
 			.build();
 		return customer;
 	}
--- square/src/main/java/com/hihi/square/domain/user/entity/Customer.java
+++ square/src/main/java/com/hihi/square/domain/user/entity/Customer.java
@@ -5,34 +5,25 @@ import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Entity
-@Data
-@SuperBuilder
+	@SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name="customer")
-@DiscriminatorValue("UA")
+@DiscriminatorValue("UA01")
 public class Customer extends User{
 
 	@Enumerated(EnumType.STRING)
 	@Column(name="`rank`")
-	private UserRank rank;
+	private UserRankType rank;
 
 	@Enumerated(EnumType.STRING)
-	private UserSocialLogin social;
+	private UserSocialLoginType social;
 
 }
