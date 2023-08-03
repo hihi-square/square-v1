ab417bc50dbfcde283439d23741138e934bd3d94
soyeonnnb <la28s5d@naver.com>
2023-08-04 01:41:54 +0900

    Backup: 0803 집

---
+++ square/src/main/java/com/hihi/square/domain/sale/dto/request/SaleCreateRequestDto.java
@@ -0,0 +1,18 @@
+package com.hihi.square.domain.sale.dto.request;
+
+import lombok.Data;
+import net.bytebuddy.matcher.FilterableList;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+@Data
+public class SaleCreateRequestDto {
+    private String name; // 타임세일 명
+    private LocalDateTime startAt; // 시작 일/시간
+    private LocalDateTime finishedAt; // 마지막 일/시간
+
+
+    private Integer price; // 총 가격 -> 팔 가격
+    private List<SaleCreateMenuRequestDto> menus;
+}
