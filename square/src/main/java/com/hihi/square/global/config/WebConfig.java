5c787d0b0da0555efdd4cfbae8f8990f8eb6cea4
soyeonnnb <la28s5d@naver.com>
2023-08-03 17:51:16 +0900

    Feat: CORS 기본포트  추가

--- square/src/main/java/com/hihi/square/global/config/WebConfig.java
+++ square/src/main/java/com/hihi/square/global/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			                .allowedOrigins("http://localhost:3000", "http://i9b208.p.ssafy.io:80", "http://43.201.255.188:80")
+			                .allowedOrigins("http://localhost:3000", "http://i9b208.p.ssafy.io:80",  "http://i9b208.p.ssafy.io","http://43.201.255.188:80")
 //			.allowedOrigins("*")
 			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 			.allowedHeaders("*")
