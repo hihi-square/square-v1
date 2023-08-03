701349211bfac4ef91ab049b4c9fd5f4d46d31b4
jihee9945 <jihee9945@naver.com>
2023-08-04 00:10:05 +0900

    refactor: webconfig 수정

--- square/src/main/java/com/hihi/square/global/config/WebConfig.java
+++ square/src/main/java/com/hihi/square/global/config/WebConfig.java
@@ -5,21 +5,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import com.hihi.square.domain.menu.intercept.StoreInterceptor;
 
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(new StoreInterceptor()) // LogInterceptor 등록
-			.order(1)    // 적용할 필터 순서 설정
-			.addPathPatterns("/store/**")
-			.excludePathPatterns("/error"); // 인터셉터에서 제외할 패턴
-	}
+	// @Override
+	// public void addInterceptors(InterceptorRegistry registry) {
+	// 	registry.addInterceptor(new StoreInterceptor()) // LogInterceptor 등록
+	// 		.order(1)    // 적용할 필터 순서 설정
+	// 		.addPathPatterns("/store/**")
+	// 		.excludePathPatterns("/error"); // 인터셉터에서 제외할 패턴
+	// }
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
