d1385b1b2ed095e74b14387093a68130cf94e304
jihee9945 <jihee9945@naver.com>
2023-08-14 19:39:50 +0900

    fix: Update connect content

--- square/src/main/java/com/hihi/square/global/sse/SseService.java
+++ square/src/main/java/com/hihi/square/global/sse/SseService.java
@@ -38,8 +38,14 @@ public class SseService {
 
 		// 503 에러를 방지하기 위한 더미 이벤트 전송
 		String eventId = makeTimeIncludeId(memberId);
-		sendNotification(emitter, eventId, emitterId, "message",
-			"eventStream Created. [userId=" + memberId + "]");
+
+		Notification notification = createNotification(null, NotificationType.READY,
+			"eventStream Created. [userId=" + memberId + "]", "");
+
+		sendNotification(emitter, eventId, emitterId, "message", new NotificationResponseDto(notification));
+
+		// sendNotification(emitter, eventId, emitterId, "message",
+		// 	"eventStream Created. [userId=" + memberId + "]");
 
 		// 클라이언트가 미수신한 Event 목록이 존재할 경우 전송하여 Event 유실을 예방
 		if (hasLostData(lastEventId)) {
